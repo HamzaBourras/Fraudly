fb58ac45ade161a85e4d3ede9546ac3c0b195f22
ELHAID Yousef <elhaidyousef@gmail.com>
2026-06-02 01:33:37 +0100

    inscription

--- Backend-Spring/authentification-service/src/main/java/net/enset/authentificationservice/security/jwt/JwtFilter.java
+++ Backend-Spring/authentification-service/src/main/java/net/enset/authentificationservice/security/jwt/JwtFilter.java
@@ -27,6 +27,12 @@ public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtils jwtUtils;
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/api/auth/") || path.startsWith("/swagger-ui") || path.startsWith("/v3/api-docs");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain)
